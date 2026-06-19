a061e39f0dac6951abfe46213002950b4ab01b72
mailmeajaylakshmanan-cmd <mailmeajay.lakshmanan@gmail.com>
2026-06-19 21:06:54 +0530

    fix: place tailwind and postcss config files at project root to ensure compiling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './index.html',
-    './src/**/*.{js,jsx}',
     './client/index.html',
     './client/src/**/*.{js,jsx}'
   ],
